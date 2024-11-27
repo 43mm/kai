14c0982863d8fd9b209ea3cce45dd27530e278ad
43mm <18689574+43mm@users.noreply.github.com>
2024-11-27 18:38:19 +0900

    feat: set new tab page

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import App from "./pages/App.svelte";
+import { mount } from "svelte";
+
+mount(App, { target: document.body });
---
+++ src/pages/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    console.log("Hello from the extension!");
+</script>
+
+<div>
+    <h1>開</h1>
+    <p>
+        Template: <code>svelte-ts</code>
+    </p>
+</div>
+
+<style>
+</style>
